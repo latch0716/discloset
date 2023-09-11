32cdfcad8dec626cba877cd25ca3db7816269e9e
latch0716 <latch0716@gmail.com>
2023-09-11 17:36:50 +0900

    バリデーションの修正
    スペルミスを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :image, presence: true
-  varidates :category, presence: true
-  varidates :color, presence: true
-  varidates :purchase_date, presence: true
+  validates :category, presence: true
+  validates :color, presence: true
+  validates :purchase_date, presence: true
 
   scope :sort_new, -> {order(created_at: :desc)}
   scope :sort_old, -> {order(created_at: :asc)}
